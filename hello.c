0ea328d681a63916453871451ccca599413efcd2
lukersw <lwalsdorf2@huskers.unl.com>
2022-08-25 19:36:42 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Luke Walsdorf and Wes Erickson
+ *         lwalsdorf2@huskers.unl.com
+ * Date: 2022/08/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+  printf("Luke Walsdorf and Wes Erickson\n");
+
+  return 0;
+}
