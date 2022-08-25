32df92439aef22f79ebf7fcc11c0fae3bb7e9ecf
lukersw <lwalsdorf2@huskers.unl.com>
2022-08-25 20:03:49 +0000

    added my major to the output

--- hello.c
+++ hello.c
@@ -12,7 +12,8 @@
 int main(int argc, char **argv) {
 
   printf("Hello World!\n");
-  printf("Luke Walsdorf and Wes Erickson\n");
+  printf("Luke Walsdorf and Wes Erickson!\n");
+  printf("Computer Science\n");
 
   return 0;
 }
